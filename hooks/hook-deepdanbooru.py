9d1bc10b10996529bce20025b6b4091613553691
zinshin36 <leewd52@gmail.com>
2026-02-17 14:50:07 -0500

    Update hook-deepdanbooru.py

--- hooks/hook-deepdanbooru.py
+++ hooks/hook-deepdanbooru.py
@@ -1,4 +1,3 @@
 from PyInstaller.utils.hooks import collect_all
 
-# Collect all DeepDanbooru files
-datas, binaries, hiddenimports = collect_all("deepdanbooru")
+datas, binaries, hiddenimports = collect_all('deepdanbooru')
